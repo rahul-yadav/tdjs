a0de079cbeb1866b16ad7c5cdca97a7335385791
Rahul Yadav <ryadav@zimbra.com>
2014-12-08 19:07:44 +0530

    Server Port number

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -11,7 +11,7 @@ exports.tearDown = function(done){
 
 
 exports.test_serverReturnsHelloWorld = function(test) {
-	server.start();
+	server.start(9000);
 	var request = http.get("http://localhost:9000"); 
 	request.on("response", function(response){
 		var receivedData = false;
--- src/server/server.js
+++ src/server/server.js
@@ -3,13 +3,13 @@
 var http = require("http");
 var	server;
 
-exports.start = function(){
+exports.start = function(portNumber){
     server = http.createServer();
     server.on('request', function(request, response){
         response.end("Hello World");
     });
 
-    server.listen(9000);
+    server.listen(portNumber);
 };
 
 exports.stop = function (callback){
