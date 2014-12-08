dc34ffe2defcabafcf8278d1f2da8032179d50d0
Rahul Yadav <ryadav@zimbra.com>
2014-12-08 19:06:01 +0530

    Server response test

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,6 +1,32 @@
 "use strict";
-var server  = require('./server.js');
-exports.testNothing = function(test) {
-    test.equals(3, server.number(), "number");
-    test.done();
+
+var server = require('./server.js');
+var http = require("http");
+
+exports.tearDown = function(done){
+	server.stop(function(){
+		done();
+	});
 };
+
+
+exports.test_serverReturnsHelloWorld = function(test) {
+	server.start();
+	var request = http.get("http://localhost:9000"); 
+	request.on("response", function(response){
+		var receivedData = false;
+		response.setEncoding("utf8");
+		test.equals(200, response.statusCode, "status code");
+		response.on("data", function(chunk) {
+			// console.log('hi');
+			receivedData = true;
+			test.equals("Hello World", chunk, "response text");
+		});
+
+		response.on("end", function() {
+			test.ok(receivedData, "should have received response data");
+			test.done();
+		});
+	});
+};
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,17 @@
 "use strict";
 
-exports.number = function(){
-    return 3;
+var http = require("http");
+var	server;
+
+exports.start = function(){
+    server = http.createServer();
+    server.on('request', function(request, response){
+        response.end("Hello World");
+    });
+
+    server.listen(9000);
+};
+
+exports.stop = function (callback){
+	server.close(callback);
 };
